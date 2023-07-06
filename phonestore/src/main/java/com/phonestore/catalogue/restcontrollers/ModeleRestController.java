e923ed7be8057a9361701fae69dfefd71408d179
remiNdong <ndongremi@aol.fr>
2023-07-06 18:57:42 +0200

    ajout du CrossOrigin sur les controllers

--- phonestore/src/main/java/com/phonestore/catalogue/restcontrollers/ModeleRestController.java
+++ phonestore/src/main/java/com/phonestore/catalogue/restcontrollers/ModeleRestController.java
@@ -3,6 +3,7 @@ package com.phonestore.catalogue.restcontrollers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import com.phonestore.catalogue.service.CatalogueService;
 
 @RestController
 @RequestMapping("/api/modeles")
+@CrossOrigin
 public class ModeleRestController {
 
 	@Autowired
