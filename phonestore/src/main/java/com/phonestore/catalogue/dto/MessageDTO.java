bf0ffd9c6a29e5eaf3a9126677000a4528deee5a
remiNdong <ndongremi@aol.fr>
2023-08-21 19:47:40 +0200

    ajout commentaire

--- phonestore/src/main/java/com/phonestore/catalogue/dto/MessageDTO.java
+++ phonestore/src/main/java/com/phonestore/catalogue/dto/MessageDTO.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * An (error) message.
+ * classe utilisée pour informer de la reussite ou l'echec d'une insertion ou une modification d'instance
+ * modeletelephone ou association ou prestation
+ * Cet objet sera donc renvoyer dans une reponse http suite à un appel de l'api Rest
  */
 
 public class MessageDTO {
